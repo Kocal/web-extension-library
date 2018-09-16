88dd88cdba4bb3e41a195bb1e35d87d1b2acf230
Hugo Alliaume <kocal@live.fr>
2018-09-16 15:55:38 +0200

    chore: configure Jest and TypeScript

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.test.json',
+    },
+  },
+  collectCoverage: !!process.env.CI,
+  coverageDirectory: 'coverage',
+};
