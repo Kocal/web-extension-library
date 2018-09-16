d1681591ff5a18be65011da657c535be9275b915
Hugo Alliaume <kocal@live.fr>
2018-09-16 21:07:56 +0200

    feat(browser-action): implement "onBadgeClicked"

--- src/browser-action/index.ts
+++ src/browser-action/index.ts
@@ -1,5 +1,6 @@
 export { markAsOffline } from './markAsOffline';
 export { markAsOnline } from './markAsOnline';
+export { onBadgeClicked } from './onBadgeClicked';
 export { setBadgeColor } from './setBadgeColor';
 export { setBadgeText } from './setBadgeText';
 export { setBadgeTitle } from './setBadgeTitle';
--- src/browser-action/setActionTitle.ts
+++
@@ -1,1 +0,0 @@
-export const setActionTitle = (title: string): void => global.console.log({ title });
