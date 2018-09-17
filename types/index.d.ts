d5d5add707e436e1d774434b5ad25562a8c0841c
Hugo Alliaume <kocal@live.fr>
2018-09-17 19:06:21 +0200

    chore: well, finally we will automatically generate types definition

--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@kocal/web-extension-library' {
-  const markAsOffline: () => void;
-  const markAsOnline: () => void;
-  const onBadgeClicked: (callback: (tab: chrome.tabs.Tab) => void) => void;
-  const setBadgeColor: (color: string | chrome.browserAction.ColorArray, tabId?: number) => void;
-  const setBadgeText: (text: string, tabId?: number) => void;
-  const setBadgeTitle: (titles: string, tabId?: number) => void;
-}
