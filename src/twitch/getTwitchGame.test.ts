2ac569b98a9df8651ddd3af4872f4866d3537d17
Hugo Alliaume <kocal@live.fr>
2018-09-20 22:31:02 +0200

    feat(twitch): implement "getTwitchGame" (#36)

---
+++ src/twitch/getTwitchGame.test.ts
@@ -0,0 +1,55 @@
+import { getTwitchGame, registerTwitchApiKeys } from '.';
+
+registerTwitchApiKeys(['82ehz5kq1xe2nha3b4z9qjnjn4tim8t']);
+
+describe('getTwitchGame', () => {
+  const spySetItem = jest.spyOn(Storage.prototype, 'setItem');
+  const spyGetItem = jest.spyOn(Storage.prototype, 'getItem');
+
+  beforeEach(() => {
+    spySetItem.mockClear();
+    spyGetItem.mockClear();
+  });
+
+  it('should works', done => {
+    getTwitchGame('21779').then(fetchedGame => {
+      const game = {
+        id: '21779',
+        name: 'League of Legends',
+        box_art_url: 'https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-{width}x{height}.jpg',
+      };
+
+      expect(localStorage.getItem).toHaveBeenLastCalledWith('_twitch_game_21779');
+      expect(localStorage.getItem).toHaveLastReturnedWith(null);
+      expect(localStorage.setItem).toHaveBeenLastCalledWith('_twitch_game_21779', JSON.stringify(game));
+      expect(fetchedGame).toEqual(game);
+
+      done();
+    });
+  });
+
+  it('should read game from local storage', done => {
+    getTwitchGame('21779').then(fetchedGame => {
+      const game = {
+        id: '21779',
+        name: 'League of Legends',
+        box_art_url: 'https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-{width}x{height}.jpg',
+      };
+
+      expect(localStorage.getItem).toHaveBeenLastCalledWith('_twitch_game_21779');
+      expect(localStorage.getItem).toHaveLastReturnedWith(JSON.stringify(game));
+      expect(localStorage.setItem).not.toHaveBeenCalled();
+      expect(fetchedGame).toEqual(game);
+
+      done();
+    });
+  });
+
+  it('should throw an error if game does not exist', done => {
+    getTwitchGame('12345678').catch((err: Error) => {
+      expect(err).toEqual(new Error(`Twitch Game "12345678" does not exist.`));
+
+      done();
+    });
+  });
+});
--- src/twitch/index.ts
+++ src/twitch/index.ts
@@ -1,2 +1,3 @@
+export { getTwitchGame } from './getTwitchGame';
 export { registerTwitchApiKeys } from './registerTwitchApiKeys';
 export { pickTwitchApiKey } from './pickTwitchApiKey';
