3f41d4854d380860a373628c4b560e58cde932f7
Hugo Alliaume <kocal@live.fr>
2018-09-21 21:48:45 +0200

    configure VuePress, prepare files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,29 @@
 module.exports = {
-  title: 'Library for web extensions',
+  title: 'Library, for web extensions',
   description: 'A set of functions used to easily write a web extension',
   base: '/library',
+  serviceWorker: true,
+  themeConfig: {
+    repo: 'Kocal-Web-Extensions/library',
+    editLinks: true,
+    docsDir: 'docs',
+    sidebar: [
+      {
+        title: 'Browser features',
+        collapsable: false,
+        children: [
+          'browser-action',
+          'notifications',
+          'tabs',
+        ],
+      },
+      {
+        title: 'Other',
+        collapsable: false,
+        children: [
+          'twitch',
+        ],
+      },
+    ],
+  },
 };
