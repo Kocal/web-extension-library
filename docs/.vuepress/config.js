c9f422969f630bc7ae4dfc87f32c79dfa074bf13
Hugo Alliaume <kocal@live.fr>
2018-09-23 21:42:47 +0200

    doc: add section for "storage"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
           'browser-features/browser-action',
           'browser-features/notifications',
           'browser-features/tabs',
+          'browser-features/storage',
         ],
       },
       {
